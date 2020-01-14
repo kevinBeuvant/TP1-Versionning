37113429c2b9a404733ab44c0d9cdbf2b333360e
Kevin Beuvant <kevin.beuvant@gmail.com>
2020-01-14 16:17:41 +0100

    add partner name

--- src/application.bat
+++
@@ -1,5 +0,0 @@
-### application.bat - 2020 - BEUVANT Kevin###
-
-echo "version 1.1"
-echo "Copyright company - 2020"
-echo "Build: 10:00"
---
+++ test/test_app.bat
@@ -0,0 +1,5 @@
+### test_app.bat - 2020 - BEUVANT Kevin###
+
+echo "version 1.2"
+echo "Copyright company - 2020"
+echo "Build: 10:00"
